100e54dfc2986b62c5a5204a741192f6b0a4eb78
Roberto Nobrega <rwnobrega@gmail.com>
2015-09-27 21:19:49 -0300

    Implement MLT-3 line coding

--- signaling.py
+++ signaling.py
@@ -64,8 +64,36 @@ class AMI_Signaling(SignalingScheme):
         return np.abs(x).astype(int)
 
 
+class MLT3_Signaling(SignalingScheme):
+    def __init__(self):
+        self.thresholds = [-0.5, 0.5]
+        self.values = [-1.0, 0.0, 1.0]
+        SignalingScheme.__init__(self)
+
+    def encode(self, bits):
+        x = np.zeros(np.size(bits))
+        state = 0
+        # State sequence: 0 (LM) -> 1 (MH) -> 2 (HM) -> 3 (ML) -> 0 (LM)
+        for i, b in enumerate(bits):
+            if b == 0:
+                x[i] = x[i - 1]  # Note that if i = 0, then i - 1 = -1,
+                                 # and x[-1] = 0 at this point
+            else:
+                state = (state + 1) % 4
+                x[i] = [0, 1, 0, -1][state]
+        return x
+
+    def decode(self, x):  # Not optimal!
+        bits_hat = np.zeros(np.size(x))
+        x_hat = self.detector.detect(x)
+        for i in range(len(x_hat)):
+            bits_hat[i] = (x_hat[i] != x_hat[i - 1]).astype(int)
+        return bits_hat
+
+
 collection = collections.OrderedDict([
     ('Unipolar', Unipolar_Signaling()),
     ('Polar', Polar_Signaling()),
-    ('AMI', AMI_Signaling())
+    ('AMI', AMI_Signaling()),
+    ('MLT-3', MLT3_Signaling())
 ])
